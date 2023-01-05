f11b40ee762a82d05e1d046adf791f04b3fa7210
Iván Lorenzo Mediavilla <ilorenzo@toools.es>
2023-01-05 16:16:42 +0100

    added inline text interpeter and command filter

--- backend/bot/bot_message_handler.py
+++ backend/bot/bot_message_handler.py
@@ -1,8 +1,10 @@
-from telegram.ext import CommandHandler
+from telegram.ext import CommandHandler, InlineQueryHandler, MessageHandler, filters
 
-from backend.bot.bot_stagges import s001_start, s002_initialize
+from backend.bot.bot_stagges import s001_start, s002_first_picture, s003_key_listener, unknown
 
 
 def bot_message_manager(my_bot):
     my_bot.add_handler(CommandHandler("start", s001_start))
-    my_bot.add_handler(CommandHandler("game", s002_initialize))
+    my_bot.add_handler(CommandHandler("game", s002_first_picture))
+    my_bot.add_handler(MessageHandler(filters.TEXT, s003_key_listener))
+    my_bot.add_handler(MessageHandler(filters.COMMAND, unknown))
--- backend/bot/bot_stagges.py
+++ backend/bot/bot_stagges.py
@@ -1,4 +1,7 @@
-from telegram import Update, KeyboardButton, KeyboardButtonPollType, ReplyKeyboardMarkup
+from uuid import uuid4
+
+from telegram import Update, KeyboardButton, KeyboardButtonPollType, ReplyKeyboardMarkup, InputTextMessageContent, \
+    InlineQueryResultArticle
 from telegram.ext import ContextTypes
 
 from backend.utils.framex.bisector import Bisector
@@ -11,7 +14,7 @@ async def s001_start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None
     )
 
 
-async def s002_initialize(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+async def s002_first_picture(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     # Load out the frame for send it to the bot
     bisector = Bisector()
     image = bisector.image
@@ -20,3 +23,22 @@ async def s002_initialize(update: Update, context: ContextTypes.DEFAULT_TYPE) ->
     await update.effective_message.reply_photo(
         photo=image, caption=message, reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
     )
+
+
+async def s003_key_listener(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    query = update.effective_message.text
+    if not query:
+        await update.message.reply_text(
+            "I don't have any answer. Do you want to try my /game of rockets?."
+        )
+    if query != 'Yes' and query != 'No':
+        await update.message.reply_text(
+            "I don't understand you, I only accept a Yes or No answer.\n"
+            "Sorry :-("
+        )
+    else:
+        await s002_first_picture(update, context)
+
+
+async def unknown(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command.")
